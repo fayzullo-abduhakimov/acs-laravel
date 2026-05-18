17f3bf5e5535ee0e540d299dd57ee37fdfd7bd92
fayzullo-abduhakimov <f.abduhakimov@acdf.uz>
2026-05-18 11:18:55 +0500

    Remove undefined properties

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,33 +3,39 @@
     use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
     // Replicates Yii2 asset manager's `appendTimestamp => true`: cache-bust
-    // local CSS/JS by file mtime so browsers never serve a stale copy.
-    $asset = function (string $path): string {
-        $full = public_path(ltrim($path, '/'));
-        return $path . (is_file($full) ? '?v=' . filemtime($full) : '');
-    };
+// local CSS/JS by file mtime so browsers never serve a stale copy.
+$asset = function (string $path): string {
+    $full = public_path(ltrim($path, '/'));
+    return $path . (is_file($full) ? '?v=' . filemtime($full) : '');
+};
 
-    $locale = app()->getLocale();
+$locale = app()->getLocale();
 
-    $seoPage = \Illuminate\Support\Facades\Cache::remember('seo.page.home', 3600, fn () => Page::where('name', 'home')->first());
+$seoPage = \Illuminate\Support\Facades\Cache::remember(
+    'seo.page.home',
+    3600,
+    fn() => Page::where('name', 'home')->first(),
+);
 
-    $seoTitle = $seoPage
-        ? ($seoPage->getTranslation('meta_title', $locale) ?: $seoPage->getTranslation('title', $locale))
-        : config('app.name');
-    $seoDescription = $seoPage ? $seoPage->getTranslation('meta_description', $locale) : null;
+$seoTitle = $seoPage
+    ? ($seoPage->getTranslation('meta_title', $locale) ?:
+    $seoPage->getTranslation('title', $locale))
+    : config('app.name');
+$seoDescription = $seoPage ? $seoPage->getTranslation('meta_description', $locale) : null;
 
-    $ogLogoMap = [
-        'ru' => '/images/logo_home/logo_home_new_ru.png',
-        'uz' => '/images/logo_home/logo_home_new_uz.png',
-        'kk' => '/images/logo_home/logo_home_new_ka.png',
-        'en' => '/images/logo_home/logo_home_new_en.png',
-    ];
-    $ogImage = url($ogLogoMap[$locale] ?? $ogLogoMap['en']);
+$ogLogoMap = [
+    'ru' => '/images/logo_home/logo_home_new_ru.png',
+    'uz' => '/images/logo_home/logo_home_new_uz.png',
+    'kk' => '/images/logo_home/logo_home_new_ka.png',
+    'en' => '/images/logo_home/logo_home_new_en.png',
+];
+$ogImage = url($ogLogoMap[$locale] ?? $ogLogoMap['en']);
 
-    $yandexMetrika = site_setting('yandex_metrika', '103799675');
+$yandexMetrika = site_setting('yandex_metrika', '103799675');
 @endphp
 <!DOCTYPE html>
 <html lang="{{ $locale }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -46,7 +52,8 @@
     <meta property="og:type" content="website">
 
     @foreach (LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-        <link rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, url()->current()) }}">
+        <link rel="alternate" hreflang="{{ $localeCode }}"
+            href="{{ LaravelLocalization::getLocalizedURL($localeCode, url()->current()) }}">
     @endforeach
 
     <!-- Favicon -->
@@ -69,44 +76,63 @@
     @if ($yandexMetrika)
         <!-- Yandex.Metrika counter -->
         <script type="text/javascript">
-            (function(m,e,t,r,i,k,a){
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id={{ $yandexMetrika }}', 'ym');
-            ym({{ $yandexMetrika }}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            (function(m, e, t, r, i, k, a) {
+                m[i] = m[i] || function() {
+                    (m[i].a = m[i].a || []).push(arguments)
+                };
+                m[i].l = 1 * new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                    if (document.scripts[j].src === r) {
+                        return;
+                    }
+                }
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+                    k, a)
+            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id={{ $yandexMetrika }}', 'ym');
+            ym({{ $yandexMetrika }}, 'init', {
+                ssr: true,
+                webvisor: true,
+                clickmap: true,
+                ecommerce: "dataLayer",
+                accurateTrackBounce: true,
+                trackLinks: true
+            });
         </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/{{ $yandexMetrika }}" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <noscript>
+            <div><img src="https://mc.yandex.ru/watch/{{ $yandexMetrika }}" style="position:absolute; left:-9999px;"
+                    alt="" /></div>
+        </noscript>
         <!-- /Yandex.Metrika counter -->
     @endif
 
     @stack('styles')
 </head>
+
 <body class="lang-{{ $locale }}">
 
-<main>
-    <div class="wrapper d-flex">
-        @yield('content')
-    </div>
+    <main>
+        <div class="wrapper d-flex">
+            @yield('content')
+        </div>
 
-    @yield('footer')
-</main>
+        @yield('footer')
+    </main>
 
-@include('site._registration_form')
+    @include('site._registration_form')
 
-<script src="{{ $asset('/js/jquery.min.js') }}"></script>
-<script src="{{ $asset('/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ $asset('/js/swiper-bundle.min.js') }}"></script>
-<script src="{{ $asset('/js/jquery.fancybox.min.js') }}"></script>
-<script>
-    window.ROUTES = {
-        getPrograms: '{{ route('get-programs') }}',
-        getArticle:  '{{ route('get-article') }}',
-        archiveYear: '{{ route('archive-year') }}'
-    };
-</script>
-<script src="{{ $asset('/js/main.js') }}"></script>
-@stack('scripts')
+    <script src="{{ $asset('/js/jquery.min.js') }}"></script>
+    <script src="{{ $asset('/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ $asset('/js/swiper-bundle.min.js') }}"></script>
+    <script src="{{ $asset('/js/jquery.fancybox.min.js') }}"></script>
+    <script>
+        window.ROUTES = {
+            getPrograms: '{{ route('get-programs') }}',
+            getArticle: '{{ route('get-article') }}',
+            archiveYear: '{{ route('archive-year') }}'
+        };
+    </script>
+    <script src="{{ $asset('/js/main.js') }}"></script>
+    @stack('scripts')
 </body>
+
 </html>
